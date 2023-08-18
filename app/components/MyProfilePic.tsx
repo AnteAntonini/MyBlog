ce6550022d1bf9530fc3205163446c35039646b5
AnteAntonini <aantonini@combis.hr>
2023-08-18 15:59:40 +0200

    resize profile picture aka logo

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -6,8 +6,8 @@ export default function MyProfilePic() {
       <Image
         className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
         src="/images/logo.svg"
-        width={200}
-        height={200}
+        width={100}
+        height={100}
         alt="Ante Antonini"
         priority={true}
       />
