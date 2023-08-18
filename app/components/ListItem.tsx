6bfa686cdbfef5396e2cc912481f01404e23dc2c
AnteAntonini <aantonini@combis.hr>
2023-08-18 15:49:29 +0200

    add hover effects, add divider, change colors to subtitle and date

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -9,25 +9,26 @@ export default function ListItem({ post }: Props) {
   const { id, title, subtitle, date } = post;
 
   return (
-    <li className="mt-4 text-2xl dark:text-white/90">
+    <li className="mt-5 text-2xl dark:text-white/90 border-b last:border-b-0 first:border-b-0">
       <Link
-        className="underline hover:text-black/70 dark:hover:text-white"
+        className="hover:text-blue-400 dark:hover:text-blue-500 transition duration-200 font-bold"
         href={`/posts/${id}`}
       >
         {title}
       </Link>
-      <p className="text-sm my-1">{subtitle}</p>
+      <p className="text-sm text-slate-300 my-1">{subtitle}</p>
       <br />
       {date && (
-        <div className="flex items-center justify-between">
-          <div className="flex items-center dark:text-blue-400">
-            <Link className="mr-2" href={`/posts/${id}`}>
-              Read more
-            </Link>
-            <AiOutlineArrowRight />
-          </div>
+        <div className="flex items-center justify-between pb-4">
+          <Link
+            className="flex items-center dark:text-blue-400 hover:text-blue-500 hover:text-blue-500 transition duration-200 text-base"
+            href={`/posts/${id}`}
+          >
+            Read more
+            <AiOutlineArrowRight className="ml-2 w-4 hover:scale-x-300" />
+          </Link>
 
-          <p className="text-sm ">{date}</p>
+          <p className="text-base text-slate-300">{date}</p>
         </div>
       )}
     </li>
