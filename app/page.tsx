347a2603e4bf9deed474c2ffd58c8465d1477521
AnteAntonini <aantonini@combis.hr>
2023-08-18 15:59:03 +0200

    edit home page, update description and surname

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ export default function Home() {
       <p className="mt-12 text-3xl text-center dark:text-white">
         Hello and Welcome 👋&nbsp;
         <span className="whitespace-nowrap">
-          I&apos;m <span className="font-bold">Ante</span>
+          I&apos;m <span className="font-bold">Ante Antonini</span>
+        </span>
+        <br />
+        <span className="text-xl text-slate-300">
+          Software engineer at <span className="text-rose-500">Combis</span>
         </span>
       </p>
       <Posts />
